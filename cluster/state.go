97d1c3d0094450316dc456737eac90f0bede97c5
Lino <lino@lino.io>
2013-06-20 06:56:40 +0200

    changed ugly method name. BREAKING!

--- cluster/state.go
+++ cluster/state.go
@@ -5,7 +5,7 @@ import (
 )
 
 // http://www.elasticsearch.org/guide/reference/api/admin-cluster-update-settings.html
-func State(settingType string, key string, value int) error {
+func UpdateSetting(settingType string, key string, value int) error {
 	url := "/_cluster/settings"
 	m := map[string]map[string]int{settingType: map[string]int{key: value}}
 	_, err := api.DoCommand("PUT", url, m)
